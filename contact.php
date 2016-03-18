498f84fe8cc7e4491a5c627e12e0dda188ee169e
tam0203 <tvegar0203@gmail.com>
2016-03-18 21:53:00 +0100

    contact

--- contact.php
+++ contact.php
@@ -17,17 +17,13 @@ if(isset($_POST['email'])) {
 	
 	// validation expected data exists
 	if(!isset($_POST['first_name']) ||
-		!isset($_POST['last_name']) ||
 		!isset($_POST['email']) ||
-		!isset($_POST['telephone']) ||
 		!isset($_POST['comments'])) {
 		died('We are sorry, but there appears to be a problem with the form you submitted.');		
 	}
 	
 	$first_name = $_POST['first_name']; // required
-	$last_name = $_POST['last_name']; // required
 	$email_from = $_POST['email']; // required
-	$telephone = $_POST['telephone']; // not required
 	$comments = $_POST['comments']; // required
 	
 	$error_message = "";
@@ -38,9 +34,6 @@ if(isset($_POST['email'])) {
 	$string_exp = "/^[A-Za-z .'-]+$/";
   if(!preg_match($string_exp,$first_name)) {
   	$error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!preg_match($string_exp,$last_name)) {
-  	$error_message .= 'The Last Name you entered does not appear to be valid.<br />';
   }
   if(strlen($comments) < 2) {
   	$error_message .= 'The Comments you entered do not appear to be valid.<br />';
@@ -56,9 +49,7 @@ if(isset($_POST['email'])) {
 	}
 	
 	$email_message .= "First Name: ".clean_string($first_name)."\n";
-	$email_message .= "Last Name: ".clean_string($last_name)."\n";
 	$email_message .= "Email: ".clean_string($email_from)."\n";
-	$email_message .= "Telephone: ".clean_string($telephone)."\n";
 	$email_message .= "Comments: ".clean_string($comments)."\n";
 	
 	
